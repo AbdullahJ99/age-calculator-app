c3b9e09105199e478ff2beaa6c019cb0cf3a6cc5
Abdullah_J <abdullahj1899@gmail.com>
2023-08-28 19:46:40 -0400

    Fixed validations and added more error messages

--- script/main.js
+++ script/main.js
@@ -12,6 +12,12 @@ const dayError = document.querySelector(".day-error");
 const monthError = document.querySelector(".month-error");
 //Year error message
 const yearError = document.querySelector(".year-error");
+//Day title
+const dayTitle = document.querySelector(".day-title");
+//Month title
+const monthTitle = document.querySelector(".month-title");
+//Year title
+const yearTitle = document.querySelector(".year-title");
 
 const getToday = () => {
     const today = new Date();
@@ -26,22 +32,26 @@ calculateButton.addEventListener("click", function (e) {
     const year = Number(yearInput.value);
     const month = Number(monthInput.value);
     const day = Number(dayInput.value);
-    validateYear(year);
-    validateMonth(month);
-    validateDay(day);
+    console.log(validateAll(year, month, day));
 });
 
 const validateYear = (input) => {
     if (input) {
         const today = getToday();
         if ((input > 0) && (input <= today[0])) {
+            yearTitle.classList.remove("red-text");
+            yearInput.classList.remove("red-border");
             yearError.innerText = "";
             return 1;
         } else {
+            yearTitle.classList.add("red-text");
+            yearInput.classList.add("red-border");
             yearError.innerText = "Please input a past year.";
             return 0;
         }
     } else {
+        yearTitle.classList.add("red-text");
+        yearInput.classList.add("red-border");
         yearError.innerText = "Please input a year.";
         return -1;
     }
@@ -50,13 +60,19 @@ const validateYear = (input) => {
 const validateMonth = (input) => {
     if (input) {
         if ((input > 0) && (input <= 12)) {
+            monthTitle.classList.remove("red-text");
+            monthInput.classList.remove("red-border");
             monthError.innerText = "";
             return 1
         } else {
-            monthError.innerText = "Please input a year between 1-12.";
+            monthTitle.classList.add("red-text");
+            monthInput.classList.add("red-border");
+            monthError.innerText = "Please input a month between 1-12.";
             return 0;
         }
     } else {
+        monthTitle.classList.add("red-text");
+        monthInput.classList.add("red-border");
         monthError.innerText = "Please input a month.";
         return -1;
     }
@@ -65,14 +81,73 @@ const validateMonth = (input) => {
 const validateDay = (input) => {
     if (input) {
         if ((input > 0) && (input <= 31)) {
+            dayTitle.classList.remove("red-text");
+            dayInput.classList.remove("red-border");
             dayError.innerText = "";
-            return 1
+            return 1;
         } else {
+            dayTitle.classList.add("red-text");
+            dayInput.classList.add("red-border");
             dayError.innerText = "Please input a day between 1-31.";
             return 0;
         }
     } else {
+        dayTitle.classList.add("red-text");
+        dayInput.classList.add("red-border");
         dayError.innerText = "Please input a day.";
         return -1;
     }
+}
+
+const validateDayToMonth = (day, month) => {
+    const months30 = [4, 6, 9, 11];
+    const months31 = [1, 3, 5, 7, 8, 10, 12];
+    if (months30.includes(month)) {
+        if (day <= 30) {
+            dayTitle.classList.remove("red-text");
+            dayInput.classList.remove("red-border");
+            dayError.innerText = "";
+            return 1;
+        } else {
+            dayTitle.classList.add("red-text");
+            dayInput.classList.add("red-border");
+            dayError.innerText = "Must be a valid date.";
+            return 0;
+        }
+    } else if (months31.includes(month)) {
+        if (day <= 31) {
+            dayTitle.classList.remove("red-text");
+            dayInput.classList.remove("red-border");
+            dayError.innerText = "";
+            return 1;
+        } else {
+            dayTitle.classList.add("red-text");
+            dayInput.classList.add("red-border");
+            dayError.innerText = "Must be a valid date.";
+            return 0;
+        }
+    } else {
+        if (day <= 28) {
+            dayTitle.classList.remove("red-text");
+            dayInput.classList.remove("red-border");
+            dayError.innerText = "";
+            return 1;
+        } else {
+            dayTitle.classList.add("red-text");
+            dayInput.classList.add("red-border");
+            dayError.innerText = "Must be a valid date.";
+            return 0;
+        }
+    }
+}
+
+const validateAll = (year, month, day) => {
+    const validYear = validateYear(year);
+    const validMonth = validateMonth(month);
+    const validDay = validateDay(day);
+    let dayToMonth = 0;
+    if (validDay && validMonth){
+       dayToMonth = validateDayToMonth(day,month);
+    }
+    return validYear && validMonth && validDay && dayToMonth;
 }

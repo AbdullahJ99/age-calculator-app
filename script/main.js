f012b3b4dd7dd32768f6a209f8e8709242cdc13d
Abdullah_J <abdullahj1899@gmail.com>
2023-08-28 18:06:50 -0400

    Added validation for year, month, and day values

--- script/main.js
+++ script/main.js
@@ -1,50 +1,60 @@
 //Button to calculate age
 const calculateButton = document.querySelector(".calculate");
 //Day input
-const dayInput = document.querySelector(".day-input"); 
+const dayInput = document.querySelector(".day-input");
 //Month input
-const monthInput = document.querySelector(".month-input"); 
+const monthInput = document.querySelector(".month-input");
 //Year input
-const yearInput = document.querySelector(".year-input"); 
+const yearInput = document.querySelector(".year-input");
 
 const getToday = () => {
     const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    const day = today.getDate();
-    const sinceToday = [year, month, day];
-    return sinceToday;
+    const year = Number(today.getFullYear());
+    const month = Number(today.getMonth() + 1);
+    const day = Number(today.getDate());
+    return [year, month, day];
 }
-console.log(calculateButton);
-console.log(dayInput);
-console.log(monthInput);
-console.log(yearInput);
 
-calculateButton.addEventListener("click", function(e){
+calculateButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const year = yearInput.value;
-    const month = monthInput.value;
-    const day = dayInput.value;
-    console.log(`${year}, ${month}, ${day}`);
+    const year = Number(yearInput.value);
+    const month = Number(monthInput.value);
+    const day = Number(dayInput.value);
 });
 
-const validateAll = (year, month, day) => {
-    return [validateYear(year), validateMonth(month), validateDay(day)];
-}
-
 const validateYear = (input) => {
-    const today = getToday();
-    if(input > today[0]){
-        return false;
-    } else{
-        return true;
+    if (input) {
+        const today = getToday();
+        if ((input > 0) && (input <= today[0])) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return -1;
     }
 }
 
 const validateMonth = (input) => {
-    if (input > 12){
-        return false;
+    if (input) {
+        if ((input > 0) && (input <= 12)) {
+            return 1
+        } else {
+            return 0;
+        }
     } else {
-        return true;
+        return -1;
+    }
+}
+
+const validateDay = (input) => {
+    if (input) {
+        if ((input > 0) && (input <= 31)) {
+            return 1
+        } else {
+            return 0;
+        }
+    } else {
+        return -1;
     }
 }
